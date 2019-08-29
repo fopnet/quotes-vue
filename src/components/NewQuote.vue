e70470fd58e9c23f0618472371562cd536b8fcaa
Felipe Pina <fop.net@gmail.com>
2019-08-28 23:58:43 -0300

    adding  vuelidate + form validations

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -1,33 +1,40 @@
 <template>
-    <div class="row">
-        <form>
-            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
-                <label>Quote</label>
-                <textarea class="form-control" rows="3" v-model="quote"></textarea>
-            </div>
-            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
-               <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
-            </div>
-        </form>
-    </div>
+  <div class="row">
+    <form>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <label>Quote</label>
+        <textarea class="form-control" rows="3" v-model="quote" @input="$v.quote.$touch()"></textarea>
+      </div>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <button class="btn btn-primary" @click.prevent="createNew" :disabled="$v.$invalid">Add Quote</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-    export default {
-        data: function() {
-            return {
-                quote: ''
-            };
-        },
-        methods: {
-            createNew() {
-                this.$emit('quoteAdded', this.quote);
-                this.quote = '';
-            }
-        }
+import { required, minLength } from "vuelidate/lib/validators";
+
+export default {
+  data: function() {
+    return {
+      quote: ""
+    };
+  },
+  validations: {
+    quote: {
+      required,
+      minLength: minLength(3)
     }
+  },
+  methods: {
+    createNew() {
+      this.$emit("quoteAdded", this.quote);
+      this.quote = "";
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
