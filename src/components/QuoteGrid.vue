342fc38976af20b5c1a69cc09eeeced59be85e5e
Felipe Pina <fop.net@gmail.com>
2019-08-29 11:40:08 -0300

    adding  slider in the newQuote

--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,25 +1,65 @@
 <template>
-    <div class="row">
-        <app-quote v-for="(quote, index) in quotes" @click.native="deleteQuote(index)">{{ quote }}</app-quote>
-    </div>
+  <div class="row">
+    <transition-group name="slider">
+      <app-quote
+        :key="quote"
+        v-for="(quote, index) in quotes"
+        @click.native="deleteQuote(index)"
+      >{{ quote }}</app-quote>
+    </transition-group>
+  </div>
 </template>
 
 <script>
-    import Quote from './Quote.vue';
+import Quote from "./Quote.vue";
 
-    export default {
-        props: ['quotes'],
-        components: {
-            appQuote: Quote
-        },
-        methods: {
-            deleteQuote(index) {
-                this.$emit('quoteDeleted', index);
-            }
-        }
+export default {
+  props: ["quotes"],
+  components: {
+    appQuote: Quote
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit("quoteDeleted", index);
     }
+  }
+};
 </script>
 
-<style>
+<style scoped>
+/** slider transitions */
+.slider-enter {
+  opacity: 0;
+}
 
+.slider-enter-active {
+  animation: slide-in 1s ease-out forwards;
+  transition: opacity 0.5s;
+}
+
+.slider-leave {
+}
+
+.slider-leave-active {
+  animation: slide-out 1s ease-out forwards;
+  transition: opacity 1s;
+  opacity: 0;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0px);
+  }
+}
+@keyframes slide-out {
+  from {
+    transform: translateY(0px);
+  }
+  to {
+    transform: translateY(20px);
+  }
+}
 </style>
